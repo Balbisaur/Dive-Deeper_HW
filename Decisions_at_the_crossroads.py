827bc95a73d34bd89f35808520e072751484e51e
Balbisaur <albi3479@yahoo.com>
2024-04-12 23:43:48 -0500

    first commit

---
+++ Decisions_at_the_crossroads.py
@@ -0,0 +1,9 @@
+number = int(input('Enter a number? '))
+
+
+if number == 0:
+    print('the number is zero')
+elif number >= 10:
+    print('The number is positive')
+else:
+    print('the number is negative')
