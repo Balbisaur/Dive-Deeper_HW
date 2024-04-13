14315e893156c61cb95b9755b7052e6637dade4b
Balbisaur <albi3479@yahoo.com>
2024-04-13 00:06:54 -0500

    first commit

---
+++ The_greatest_showdown.py
@@ -0,0 +1,24 @@
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+num_3 = input("Enter the third number: ")
+
+
+if num_1 >= num_2:
+    print('num_1 is greater')
+elif num_2 >= num_3:
+    print('num_2 is greater')
+else:
+    print('num_3 is greater')
+
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+num_3 = input("Enter the third number: ")
+
+
+if num_1 <= num_2:
+    print('num_1 is smaller')
+elif num_2 <= num_3:
+    print('num_2 is smaller')
+else:
+    print('num_3 is smaller')
+ 
